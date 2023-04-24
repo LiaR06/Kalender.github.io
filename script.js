c24c2b61f59c9fc8bd4ce3b859deac02d5b40de9
LiaR06 <130407092+LiaR06@users.noreply.github.com>
2023-04-24 10:07:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,303 +1,257 @@
 let nav = 0;
-let clicked = null; 
+let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
+// Anlegen von Variablen zur späteren Verwendung
 const calendar = document.getElementById('calendar');
-const newEventModal = document.getElementById ('newEventModal');
-const deleteEventModal = document.getElementById ('deleteEventModal');
-const backDrop = document.getElementById ('modalBackDrop');
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
 const eventTitleInput = document.getElementById('eventTitleInput');
 const eventDescriptionInput = document.getElementById('eventDescriptionInput');
 const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-//let count= 0;  --Lia
 
-// const num = clicked.split('/')[1]; 
-// num === i - paddingDays
+// Macht Modal sichtbar
+function openModal(date) {
+  clicked = date;
 
-
-/*function appointmentCount(){
-    
-    if (element, dayString) {
-        count= 3
-    } else{
-        count=0
-    }
-    
-}  --Lia keine Sorge ich weiß dass es keinen Sinn macht, wollte das nur als Test nehmen*/
-
-function openModal(date, is_new) {
-    clicked = date;
-
-    const eventForDay = events.find(e => e.date === clicked);
-
-    if (is_new){
-        newEventModal.style.display = 'block';
-    } else {
-        document.getElementById('eventTitle').innerText = eventForDay.title;
-        document.getElementById('eventDescription').innerText = eventForDay.description;
-        deleteEventModal.style.display = 'block';
-    }
-
-    backDrop.style.display = 'block';
+  // Modal und Backdrop werden sichtbar
+  newEventModal.style.display = 'block';
+  backDrop.style.display = 'block';
 }
 
+// Öffnet bestehenden Termin
 function openAppointment(appointment) {
-    document.getElementById('eventTitle').innerText = appointment.title;
-    document.getElementById('eventDescription').innerText = appointment.description;
-    deleteEventModal.style.display = 'block';
 
-    document.getElementById('deleteButton').addEventListener('click', () => deleteAppointment(appointment));
-    backDrop.style.display = 'block';
+  // Setzt Titel und Beschreibung für geöffneten Termin
+  document.getElementById('eventTitle').innerText = appointment.title;
+  document.getElementById('eventDescription').innerText = appointment.description;
+
+  // Setzt EventListener zum Löschen des Termins
+  document.getElementById('deleteButton').addEventListener('click', () => deleteAppointment(appointment));
+
+  deleteEventModal.style.display = 'block';
+  backDrop.style.display = 'block';
 }
 
+// Lädt alle Kalendertage
+function load() {
 
-function load(){
-    const dt = new Date();
+  const dt = new Date();
+  if (nav !== 0) {
+    dt.setMonth(new Date().getMonth() + nav);
+  }
 
-    if (nav !== 0){ 
-        dt.setMonth(new Date().getMonth() + nav);
-    }
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
 
-    const day = dt.getDate();
-    const month = dt.getMonth();
-    const year = dt.getFullYear();
+  const firstDayOfMonth = new Date(year, month, 1)
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-    const firstDayOfMonth = new Date(year, month, 1)
-    const daysInMonth = new Date(year, month +1, 0).getDate();
-    
-    const dateString = firstDayOfMonth.toLocaleDateString('en-us', { 
-        weekday: 'long',
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-    });
-    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+  const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  });
+  const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 
-    document.getElementById('monthDisplay').innerHTML = 
+  // Monats Überschrift
+  document.getElementById('monthDisplay').innerHTML =
     `
-    <span class="hidden-mobile">${dt.toLocaleDateString('de-DE', { month: 'long'})} ${year}</span> 
-    <span class="hidden-desktop">${dt.toLocaleDateString('de-DE', { month: 'long'})} <p class="hidden-desktop">${year}</p></span>
+    <span class="hidden-mobile">${dt.toLocaleDateString('de-DE', { month: 'long' })} ${year}</span> 
+    <span class="hidden-desktop">${dt.toLocaleDateString('de-DE', { month: 'long' })} <p class="hidden-desktop">${year}</p></span>
     `;
 
-    calendar.innerHTML = '';
-    
-    for(let i = 1; i <= paddingDays + daysInMonth; i++) { 
-        const daySquare = document.createElement('div');
-        const dayNumber = document.createElement('span');
-        daySquare.classList.add('day');
+  // Setzt Inhalt des Kalenders initial leer
+  calendar.innerHTML = '';
 
-        const dayString = `${month + 1}/${i - paddingDays}/${year}`;
-        
-        
-        //let ACount = count(dayString);   --Lia
-        
-        
-        if (i > paddingDays) {
-            daySquare.appendChild(dayNumber);
-            dayNumber.innerHTML = i - paddingDays + `<span id="appointment_amount${i - paddingDays}"></span>`;
-            //dayNumber.innerHTML = i - paddingDays //+ " " + ACount; --Lia
-            if(i -paddingDays === day && nav === 0 ){
-                daySquare.id = 'currentDay';
-            }
-            
-            events.forEach(element => {
-                //ACount+=1  --Lia
-                if (element.date === dayString) {
-                    const eventDiv = document.createElement('div');
-                    eventDiv.classList.add('event');
-                    eventDiv.innerText = element.title;
-                    eventDiv.addEventListener('click', (e) => {
-                        e.stopPropagation();
-                        //openModal(dayString, false);
-                        openAppointment(element);
-                    });
-                    daySquare.appendChild(eventDiv);
-                }
-            });
+  // Loopt über die Anzahl an Monatstagen und erstellt die Terminkästchen
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+    const daySquare = document.createElement('div');
+    const dayNumber = document.createElement('span');
+    daySquare.classList.add('day');
 
-            daySquare.addEventListener('click', () => openModal(dayString, true));
-        } else {
-            daySquare.classList.add('padding');
+    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+    if (i > paddingDays) {
+      daySquare.appendChild(dayNumber);
+      dayNumber.innerHTML = i - paddingDays + ` <div class="amount_num" id="appointment_amount${year}${month + 1}${i - paddingDays}"></div>`;
+      if (i - paddingDays === day && nav === 0) {
+        daySquare.classList.add('currentDay');
+      }
+
+      // Erstellt die Termine des aktuellen Kästchens
+      events.forEach(element => {
+        if (element.date === dayString) {
+          const eventDiv = document.createElement('div');
+          eventDiv.classList.add('event');
+          eventDiv.innerText = element.title;
+          // Setzt EventListerner zum Öffnen des Terminmodals
+          eventDiv.addEventListener('click', (e) => {
+            e.stopPropagation();
+            openAppointment(element);
+          });
+          daySquare.appendChild(eventDiv);
         }
+      });
 
-        calendar.appendChild(daySquare);
+      // Setzt EventListener zum Öffnen des Modals zum Erstellen neuer Termine
+      daySquare.addEventListener('click', () => openModal(dayString));
+    } else {
+      daySquare.classList.add('padding');
     }
 
-    
+    // Fügt Kästchen dem Kalender hinzu
+    calendar.appendChild(daySquare);
+  }
+  // Updated alle Terminzähler
+  updateAppointmentAmount();
 }
 
+// Schließt Modal zum Erstellen neuer Termine
+function closeModal() {
+  eventTitleInput.classList.remove('error');
+  eventDescriptionInput.classList.remove('error');
+  newEventModal.style.display = 'none';
+  deleteEventModal.style.display = 'none';
+  backDrop.style.display = 'none';
+  eventTitleInput.value = '';
+  eventDescriptionInput.value = '';
+  clicked = null;
+  load();
+}
 
+// Helferfunktion um alle Kinderelemente eines Elements zu löschen
+function removeAllListItems(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
 
-function closeModal(){ 
+// Updated die Listenansicht indem neue Termin-Items erstellt und hinzugefügt werden
+function updateList() {
+  const listcontainer = document.getElementById('date-list');
+  removeAllListItems(listcontainer);
+
+  // Loopt über alle Termine im Local Storage und fügt für jeden Eintrag ein eigenes Listenelement hinzu
+  events.forEach(el => {
+    const dateItem = document.createElement('div');
+    dateItem.id = 'list_date_' + el.date;
+    dateItem.classList.add('dateItem');
+
+    const date = document.createElement('p');
+    const heading = document.createElement('p');
+    const description = document.createElement('p');
+
+    heading.classList.add('heading');
+    description.classList.add('description');
+
+    listcontainer.appendChild(dateItem);
+
+    dateItem.appendChild(date);
+    dateItem.appendChild(heading);
+    dateItem.appendChild(description);
+
+    let dateString = el.date.split("/")
+
+    date.innerHTML = dateString[1] + '.' + dateString[0] + '.' + dateString[2];
+    heading.innerHTML = el.title;
+    description.innerHTML = el.description;
+
+  });
+}
+
+// Speichert einen neuen Termin ab
+function saveEvent() {
+  if (eventTitleInput.value && eventDescriptionInput.value) {
     eventTitleInput.classList.remove('error');
     eventDescriptionInput.classList.remove('error');
-    newEventModal.style.display = 'none';
-    deleteEventModal.style.display = 'none';
-    backDrop.style.display = 'none';
-    eventTitleInput.value = '';
-    eventDescriptionInput.value = '';
-    clicked = null;
-    load();
+
+    events.push({
+      date: clicked,
+      title: eventTitleInput.value,
+      description: eventDescriptionInput.value,
+    });
+    // Speichert neues Element im Local Storage
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
+  } else if (!eventTitleInput.value && !eventDescriptionInput.value) {
+    eventTitleInput.classList.add('error');
+    eventDescriptionInput.classList.add('error');
+  } else if (eventTitleInput.value && !eventDescriptionInput.value) {
+    eventTitleInput.classList.remove('error');
+    eventDescriptionInput.classList.add('error');
+  } else if (!eventTitleInput.value && eventDescriptionInput.value) {
+    eventDescriptionInput.classList.remove('error');
+    eventTitleInput.classList.add('error');
+  }
+  updateList();
 }
 
-function updateList() {
-    const listcontainer = document.getElementById('date-list');
+// Loopt über events (alle Termine im Localstorage) und setzt die Anzahl der Termine pro Tag
+function updateAppointmentAmount() {
+  events.forEach(el => {
+    let dateString = el.date.split("/")
+    let item = document.getElementById('appointment_amount' + dateString[2] + dateString[0] + dateString[1]);
 
-    events.forEach(el => {
-        const date = document.createElement('div');
-        date.id = 'list_date_' + el.date;
-
-        const heading = document.createElement('p');
-        const description = document.createElement('p');
-
-        let check = document.getElementById('list_date_' + el.date);
-        if (check) {
-            check.appendChild(heading);
-            check.appendChild(description);
-        } else {
-            listcontainer.appendChild(date);
-            listcontainer.appendChild(heading);
-            listcontainer.appendChild(description);
-        }
-        
-        // TODO: Funktion aufrufen wenn events updatet
-        // TODO: heading und description nicht in date schreiben um styling zu ermöglichen
-        
-        date.innerHTML = el.date; 
-        heading.innerHTML = el.title;
-        description.innerHTML = el.description;
-        
-    });
-}
-/*
-function updateList2() {
-    const listcontainer2 = document.getElementById('date-list2');
-
-    events.forEach(el => {
-        const date = document.createElement('div');
-        date.id = 'list_date_' + el.date;
-
-        const heading = document.createElement('p');
-        const description = document.createElement('p');
-
-        let check = document.getElementById('list_date_' + el.date);
-        if (check) {
-            check.appendChild(heading);
-            check.appendChild(description);
-        } else {
-            listcontainer2.appendChild(date);
-            listcontainer2.appendChild(heading);
-            listcontainer2.appendChild(description);
-        }
-        
-        // TODO: Funktion aufrufen wenn events updatet
-        // TODO: heading und description nicht in date schreiben um styling zu ermöglichen
-        
-        date.innerHTML = el.date; 
-        heading.innerHTML = el.title;
-        description.innerHTML = el.description;
-        
-    });
-}*/
-
-function saveEvent(){ 
-    if (eventTitleInput.value){
-        const num = clicked.split('/')[1]; 
-       /* if(i > paddingDays){ 
-            num === i - paddingDays;
-            console.log(num);
-        }
-        // date von Termin/element muss gleich Date von Tag sein --> count +1
-        // count darf aber auch nur auf den einen Tag zugreifen und nicht auf alle Tage
-        // count=> forEach: Date vom tag herausfinden wie viele elemente mit dem gleichem Tag zu finden sind
-        // count veränderbar --> "let" und nicht "const"*/
-
-        eventTitleInput.classList.remove('error');
-
-        events.push({
-            date: clicked, 
-            title: eventTitleInput.value, 
-            description: eventDescriptionInput.value, 
-        });
-
-        localStorage.setItem('events', JSON.stringify(events));
-        
-        //count+=1; --Lia
-
-        //updateAppointmentAmount(num);
-        closeModal();
-    } else { 
-        eventTitleInput.classList.add('error');
+    // Falls Termin nicht im aktuellen Monat, setze den Zähler nicht
+    if (item == null) {
+      return;
     }
+    // Falls Terminzähler leer, setze auf "1" - Ansonsten füge +1 hinzu
+    if (item.innerHTML) {
+      item.innerHTML = parseInt(item.innerHTML) + 1;
+    } else {
+      item.innerHTML = 1;
+    }
+  });
 }
 
-/*
-function updateAppointmentAmount(id) {
-    const itemid = 'appointment_amount' + id;
-    console.log(document.getElementById(itemid));
-    document.getElementById(itemid).innerHTML = '1';
-    
-}
-*/
-
-function deleteEvent(){ 
-    events = events.filter(e => e.date !== clicked);
-    localStorage.setItem('events', JSON.stringify(events));
-
-    //count-=1;  --Lia
-
-    closeModal();
+// Löscht ein Termin aus dem Localstorage
+function deleteAppointment(appointment) {
+  events = events.filter(e => e !== appointment);
+  localStorage.setItem('events', JSON.stringify(events));
+  closeModal();
 }
 
-function deleteAppointment(appointment){
-    console.log(appointment);
-    events = events.filter(e => e !== appointment);
-    localStorage.setItem('events', JSON.stringify(events));
-    //count-=1;  --Lia
-    closeModal();
-}
-
+// Initiert die Buttons im Header und Modals, diese bekommen ihre zugehörigen EventListener
 function initButtons() {
-    document.getElementById('nextButton').addEventListener('click',() => {
-        nav++;
-        load();
-    });
+  document.getElementById('nextButton').addEventListener('click', () => {
+    nav++;
+    load();
+  });
 
-    document.getElementById('backButton').addEventListener('click',() =>{
-        nav--;
-        load();
-    });
+  document.getElementById('backButton').addEventListener('click', () => {
+    nav--;
+    load();
+  });
 
-    document.getElementById('saveButton').addEventListener('click', saveEvent);
-    document.getElementById('eventTitleInput').addEventListener("keypress", event => {
-        if (event.key === 'Enter') {saveEvent()}});
-        
-        document.getElementById('eventDescriptionInput').addEventListener("keypress", event => {
-        if (event.key === 'Enter') {saveEvent()}});
+  document.getElementById('saveButton').addEventListener('click', saveEvent);
 
-    document.getElementById('cancelButton').addEventListener('click', closeModal);
+  document.getElementById('cancelButton').addEventListener('click', closeModal);
 
+  document.getElementById('eventTitleInput').addEventListener("keypress", event => {
+    if (event.key === 'Enter') { saveEvent() }
+  });
 
+  document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    const new_modal = document.getElementById('newEventModal');
+    const delete_modal = document.getElementById('deleteEventModal');
+    if (evt.key === 'Escape' && new_modal.style.display !== 'none') {
+      closeModal();
+    }
+    if (evt.key === 'Escape' && delete_modal.style.display !== 'none') {
+      closeModal();
+    }
+  };
 
-    document.onkeydown = function(evt) {
-        evt = evt || window.event;
-        const new_modal = document.getElementById('newEventModal');
-        const delete_modal = document.getElementById('deleteEventModal');
-        if (evt.key === 'Escape' && new_modal.style.display !== 'none' ) {
-            closeModal();
-        }
-        if (evt.key === 'Escape' && delete_modal.style.display !== 'none' ) {
-            closeModal();
-        }
-    };
-
-
-
-
-    document.getElementById('closeButton').addEventListener('click', closeModal);
+  document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
-initButtons();
-updateList();
-//updateList2();
+// Initiales ausführen aller nötigen Funktionen
+initButtons()
 load();
+updateList();
